73ef047302dd8036a24dbc381d0ff094b6a29817
Code~warrior200 <61137642+code-warrior200@users.noreply.github.com>
2025-08-01 15:58:08 -0700

    git commit

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -12,17 +12,39 @@ const projects: IProjectItem[] = [
     url: "https://code-warriors-tech.github.io/school_website/",
     tags: ["React.js","Tailwind"],
   },
-  // {
-  //   id: "e-commerce-app-mern",
-  //   title: "E-commerce App",
-  //   description:
-  //     "An e-commerce web application developed using React.js, Material UI, Redux, and Stripe.",
-  //   icon: "/skills/nextjs.svg",
-  //   repoType: RepoType.Public,
-  //   githubUrl: "https://github.com/code-warrior200/nextshop",
-  //   url: "#",
-  //   tags: ["React.js", "Redux","Tailwind css", "Stripe"],
-  // },
+  {
+    id: "e-commerce-app-mern",
+    title: "E-commerce App",
+    description:
+      "A Fintech Stock trading Web App (landingpage) built with React.js, Redux, and Tailwind CSS.",
+    icon: "/skills/nextjs.svg",
+    repoType: RepoType.Public,
+    githubUrl: "https://github.com/Rabovel1/Website",
+    url: "https://www.rabovel.com/",
+    tags: ["Tailwind css", "Next.js"],
+  },
+  {
+    id: "Rabovel landing page",
+    title: "Rabovel Stock Trading landing page",
+    description:
+      "A Fintech Stock trading Web App (landingpage) built with React.js, Redux, and Tailwind CSS.",
+    icon: "/skills/nextjs.svg",
+    repoType: RepoType.Public,
+    githubUrl: "https://github.com/Rabovel1/Website",
+    url: "https://www.rabovel.com/",
+    tags: ["Tailwind css", "Next.js"],
+  },
+  {
+    id: "Faculty Journal",
+    title: "Faculty of Science education Journal",
+    description:
+      "A Journal website built with Next.js and Tailwind CSS.",
+    icon: "/skills/nextjs.svg",
+    repoType: RepoType.Public,
+    githubUrl: "https://github.com/code-warrior200/faculty-journal",
+    url: "https://faculty-journal.vercel.app/",
+    tags: ["Tailwind css", "Next.js"],
+  },
   {
     id: "Company website",
     title: "Code warriors Tech Website ",
